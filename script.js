9333fe36604c0472f48e9a7cb576510fcf7c17e8
Himanshu Singh Parmar <hp2146@gmail.com>
2021-01-17 00:26:52 +0530

    initial commit5

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// function target(){
+//     let value =
+// }
+let x = document.querySelector(".guess").value;
+// console.log(x);
+// const z = () => 5;
+document.querySelector(".check").addEventListener("click", function () {
+  //   console.log(z());
+  let x = document.querySelector(".guess").value;
+  document.querySelector(".message").textContent = x;
+});
